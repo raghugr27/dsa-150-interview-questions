33f0b73980c0d327e2e4107735ac78be9251a696
raghugr27 <raghutheperfect@gmail.com>
2026-02-06 22:24:06 +0530

    js dsa-150 candy 06Feb2026

---
+++ candy.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    let n = ratings.length;
+    let candies = Array(n).fill(1);
+
+    // Left to right
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    // Right to left
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    return candies.reduce((a, b) => a + b, 0);
+};
